bc4b773f9b896e94a7cf133cd6ce76b68902c158
Antal Farkas <farkasantal.hun@gmail.com>
2017-09-09 14:43:26 +0200

    Fixed hw back button behavior

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
-import { StatusBar } from 'react-native'
-import { addNavigationHelpers } from 'react-navigation'
+import { StatusBar, BackHandler } from 'react-native'
+import { addNavigationHelpers, NavigationActions } from 'react-navigation'
 import { connect } from 'react-redux'
 import AppNavigator from '../navigators/AppNavigator'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.onBackPress = this.onBackPress.bind(this)
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress)
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress)
+  }
+
+  onBackPress() {
+    const { dispatch, nav } = this.props
+
+    if (nav.index === 0) {
+      return false
+    }
+
+    dispatch(NavigationActions.back())
+    return true
+  }
+
   render() {
     StatusBar.setBarStyle('light-content', true)
     StatusBar.setBackgroundColor('#303F9F', true)
